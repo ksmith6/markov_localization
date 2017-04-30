3e7078e36489f701b6bbad9ef4ab834473f3f92d
Kelly Smith <ksmith433@gatech.edu>
2017-04-30 15:03:49 -0500

    Implemented first version (likely buggy) of updateWeights (untested).

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -93,6 +93,19 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
 	// NOTE: this method will NOT be called by the grading code. But you will probably find it useful to 
 	//   implement this method and use it as a helper during the updateWeights phase.
 
+	//for (int i=0; i<observations.size(); i++) {
+
+	//}
+
+	// Transform each observation from vehicle-relative coordinates into map coordinates
+	// z_global = x_particle + T(-theta) * [z_x; z_y]
+
+	// Compute distance between this observation and every landmap in the map.
+	//   If the distance is smaller than the previous distance, then store that landmark as the associated landmark.
+
+	// 
+
+
 }
 
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], 
@@ -115,17 +128,47 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 	for (int i=0; i<num_particles; i++) {
 		double w = 1.0;
 
-		// TODO - Compute likelihood of each measurements
-		
-		//double denominator = sqrt(2*pi*sigma);
-		//for (int j=0; j < num_measurements; j++) {
-		//	w *= denominator * exp(-0.5 * dxT * sigInv * dx);
-		//}
-		
-		sumW += w;
 
-		particles[i].weight = w;
-	}
+		// Cache trig operations for CPU savings.
+		double cosH = cos(-particles[i].theta);
+		double sinH = sin(-particles[i].theta);
+
+		for (int j=0; j<observations.size();j++) {
+			// Transform the observations into map coordinate system.
+			//    z_m = xp + T * z_v
+			double ox = particles[i].x + cosH * observations[j].x + sinH * observations[j].y;
+			double oy = particles[i].y - sinH * observations[j].x + cosH * observations[j].y;
+
+			// Perform landmark-observation association with nearest-neighbor search.
+			double minDist;
+			double associated_landmark_x;
+			double associated_landmark_y;
+			for (int k=0; i<map_landmarks; k++) {
+				double thisDist = dist(ox, oy, map_landmarks[k].x, map_landmarks[k].y);
+				
+				// If this is the first iteration or if the distance is less than the minimum distance, 
+				if (k==0 || (thisDist < minDist)) {
+					// Then update the minimum distance.
+					minDist = thisDist;
+					associated_landmark_x = map_landmarks[k].x;
+					associated_landmark_y = map_landmarks[k].y;
+				}
+			} // end of nearest neighbor search loop
+
+			// Now we have the closest landmark to the current observation.  
+			// Let's condition the weight by its likelihood.
+
+			// Let's evaluate the multivariate Gaussian PDF
+			double firstTerm = 1.0 / (2 * 3.14159265358979323846 * std_landmark[0] * std_landmark[1]);
+			double xComp = 0.5 * (ox - associated_landmark_x)*(ox - associated_landmark_x) / (std_landmark[0]*std_landmark[0]);
+			double yComp = 0.5 * (oy - associated_landmark_y)*(oy - associated_landmark_y) / (std_landmark[1]*std_landmark[1]);;
+			double pdf = firstTerm * exp(-(xComp + yComp)); 
+
+			particles[i].weight *= pdf;
+
+		} // end of observation processing loop
+		sumW += particles[i].weight;
+	} // end of particle loop
 
 	// Normalization step
 	for (int i=0; i < num_particles; i++) {
